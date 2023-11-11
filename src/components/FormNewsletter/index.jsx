a8ca8540142c764fdab8fb5735eee620fc0689de
Abner Joia <abnerjoia1991@gmail.com>
2023-11-11 13:00:32 -0300

    final

--- src/components/FormNewsletter/index.jsx
+++ src/components/FormNewsletter/index.jsx
@@ -43,14 +43,17 @@ export function FormNewsletter() {
           <Col lg={6}>
             <Form
               id="newsletter"
+              aria-label="Formulário para assinar newsletter"
+              tabIndex={0}
               onSubmit={(e) => handleSubmitForm(e)}
               className="m-auto m-lg-0"
             >
-              <legend>Informações pessoais</legend>
+              <legend tabIndex={0}>Informações pessoais</legend>
               <Form.Group className="mb-3">
-                <Form.Label>Nome Completo</Form.Label>
+                <Form.Label htmlFor="nomecompleto">Nome Completo</Form.Label>
                 <Form.Control
                   type="text"
+                  id="nomecompleto"
                   placeholder="Insira seu nome completo"
                   value={dataForm?.nomeCompleto}
                   onChange={(e) =>
@@ -59,20 +62,22 @@ export function FormNewsletter() {
                 />
               </Form.Group>
               <Form.Group className="mb-3">
-                <Form.Label>Seu melhor e-mail</Form.Label>
+                <Form.Label htmlFor="email">Seu melhor e-mail</Form.Label>
                 <Form.Control
                   type="email"
+                  id="email"
                   placeholder="Insira seu melhor e-mail"
                   value={dataForm?.email}
                   onChange={(e) => handleSetDataForm("email", e.target.value)}
                 />
               </Form.Group>
-              <div
+              <button
                 className="btn-form-newletter btn-form"
-                type="submit"             
+                type="submit" 
+                aria-label="Enviar formulário para assinar newsletter"            
               >
                 Assinar
-              </div>
+              </button>
             </Form>
           </Col>
         </Row>
