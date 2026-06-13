f5e00687205f5d61112251dbf3ad75b20032adcf
Abner Joia <abnerjoia1991@gmail.com>
2026-06-12 21:24:59 -0300

    chore: Atualizando data footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,7 +8,7 @@ export function Footer() {
       <Row>
         <Col className="d-flex p-3 justify-content-center align-items-center">
           <span className="span">
-            Ciclo de palestras Residência em TIC Software 2025.2
+            Ciclo de palestras Residência em TIC Software 2026.1
           </span>
         </Col>
       </Row>
